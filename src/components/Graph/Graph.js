bfa716714a5ebbcf2083eafa6a7c3e3afa23d024
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-11-25 07:58:55 +0200

    Feautre Update respnsiveness and few buttons

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import graph from './graph.jpg';
+import './graph.css'
 
 
 class Graph extends Component {
@@ -8,7 +9,20 @@ class Graph extends Component {
     return (
       <div className="row">
         <div className="col-md-12">
-            <img src={graph}/>
+            <div className="jumbotron">
+              <h3>Bar Heading Goes Here</h3>
+                <img className="img-responsive" src={graph} alt="Placeholder for Graph" />
+                <div className="btn-group pull-left">
+                  <button className="btn btn-primary btn-xs ">Plt Graph</button>
+                  <button className="btn btn-success btn-xs ">Bar Graph</button>
+                  <button className="btn btn-primary btn-xs ">Compare</button>
+                  <button className="btn btn-primary btn-xs ">Table</button>
+                </div>
+                <div className="btn-group pull-right">
+                  <button className="btn btn-primary btn-xs ">Save</button>
+                  <button className="btn btn-primary btn-xs ">Download</button>
+                </div>
+            </div>
         </div>        
       </div>
     );
