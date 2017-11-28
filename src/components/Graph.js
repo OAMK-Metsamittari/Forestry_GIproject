0bf39338d5bc1aede9bec72167d3d187e04a2d29
Jusu Lamminheimo <jusu_lamminheimo@live.fi>
2017-11-28 15:07:41 +0200

    radio buttons changing chart v0.1

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,70 +1,89 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { RadioGroup, RadioButton } from 'react-radio-buttons';
-const ReactHighcharts = require('react-highcharts');
+import Highcharts from 'highcharts';
+import axios from 'axios';
+import ReactTable from 'reacttable';
 
 
 class Graph extends Component {
+    chart1= () => {
+        Highcharts.chart('chart', {
+            chart: {
+                type: 'column'
+            },
+        
+            title: {
+                text: 'Styling axes and columns'
+            },
+        
+            yAxis: [{
+                className: 'highcharts-color-0',
+                title: {
+                    text: 'Primary axis'
+                }
+            }, {
+                className: 'highcharts-color-1',
+                opposite: true,
+                title: {
+                    text: 'Secondary axis'
+                }
+            }],
+        
+            plotOptions: {
+                column: {
+                    borderRadius: 5
+                }
+            },
+        
+            series: [{
+                data: [1, 3, 2, 4, 5]
+            }]
+        });
+    }
+
 
   render() {
-    const config = {      
-          chart: {
-              type: 'column'
-          },
+    const Url = "http://melatupa.azurewebsites.net";
+    
+            axios.get( 'https://cors-anywhere.herokuapp.com/' + Url + '/regionLevels')
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
       
-          title: {
-              text: 'Styling axes and columns'
-          },
-      
-          yAxis: [{
-              className: 'highcharts-color-0',
-              title: {
-                  text: 'Primary axis'
-              }
-          }, {
-              className: 'highcharts-color-1',
-              opposite: true,
-              title: {
-                  text: 'Secondary axis'
-              }
-          }],
-      
-          plotOptions: {
-              column: {
-                  borderRadius: 5
-              }
-          },
-      
-          series: [{
-              data: [1, 3, 2, 4, 5]
-          }, {
-              data: [324, 124, 547, 221],
-              yAxis: 1
-          }]
-          
-      
-      }
     return (
+        <div className="col-md-6">
+        <h2 className="text-center">Placeholder</h2>
+
+<div className="row">
+
+<div id="chart" className="col-md-12" > 
+    <figure className="text-left" >
+
+    </figure>
+</div>
+</div>
      <div >
       <div className="row sidespace">
-        <div className="col-md-12 well well-sm bggraph">
-           <ReactHighcharts config = {config}></ReactHighcharts>
            <RadioGroup onChange={ this.onChange } horizontal>
-      <RadioButton value="singlescenario">
+      <RadioButton value="singlescenario" onChange={this.chart1}>
       placeholder
       </RadioButton>
-      <RadioButton value="manyscenariossep">
+      <RadioButton value="manyscenariossep" onChange={this.chart1}>
       placeholder
       </RadioButton>
-      <RadioButton value="manyscenarioschart">
+      <RadioButton value="manyscenarioschart" onChange={this.chart1}>
       placeholder
       </RadioButton>
-      <RadioButton value="manyscenariosbar">
+      <RadioButton value="manyscenariosbar" onChange={this.chart1}>
       placeholder
       </RadioButton>
-    </RadioGroup>
-        </div>        
+    </RadioGroup>      
       </div>
+    </div>
     </div>
     );
   }
